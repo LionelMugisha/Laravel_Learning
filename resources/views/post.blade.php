c325b03b34944599228871d02b5d004885ab1bef
LionelMugisha <lionelmugisha2@gmail.com>
2022-01-12 10:10:09 +0200

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Blog</title>
+        <link rel="stylesheet" href="/app.css">
+    </head>
+    <body>
+
+        <article>
+            <?= $post; ?>
+        </article>
+
+        <a href="/">Go back</a>
+
+    </body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Blog</title>
+        <link rel="stylesheet" href="/app.css">
+    </head>
+    <body>
+
+        <article>
+            <h1><a href="/post/first-post">My First Post</a></h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sit libero in quidem harum dolorum praesentium rerum explicabo, vero, deleniti eum magnam recusandae illo. Suscipit, laudantium! Praesentium at aspernatur molestiae?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sit libero in quidem harum dolorum praesentium rerum explicabo, vero, deleniti eum magnam recusandae illo. Suscipit, laudantium! Praesentium at aspernatur molestiae?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sit libero in quidem harum dolorum praesentium rerum explicabo, vero, deleniti eum magnam recusandae illo. Suscipit, laudantium! Praesentium at aspernatur molestiae?
+            </p>
+        </article>
+
+        <article>
+            <h1><a href="/post/second-post">My Second Post</a></h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sit libero in quidem harum dolorum praesentium rerum explicabo, vero, deleniti eum magnam recusandae illo. Suscipit, laudantium! Praesentium at aspernatur molestiae?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sit libero in quidem harum dolorum praesentium rerum explicabo, vero, deleniti eum magnam recusandae illo. Suscipit, laudantium! Praesentium at aspernatur molestiae?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sit libero in quidem harum dolorum praesentium rerum explicabo, vero, deleniti eum magnam recusandae illo. Suscipit, laudantium! Praesentium at aspernatur molestiae?
+            </p>
+        </article>
+
+        <article>
+            <h1><a href="/post/third-post">My Third Post</a></h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sit libero in quidem harum dolorum praesentium rerum explicabo, vero, deleniti eum magnam recusandae illo. Suscipit, laudantium! Praesentium at aspernatur molestiae?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sit libero in quidem harum dolorum praesentium rerum explicabo, vero, deleniti eum magnam recusandae illo. Suscipit, laudantium! Praesentium at aspernatur molestiae?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sit libero in quidem harum dolorum praesentium rerum explicabo, vero, deleniti eum magnam recusandae illo. Suscipit, laudantium! Praesentium at aspernatur molestiae?
+            </p>
+        </article>
+
+    </body>
+</html>
