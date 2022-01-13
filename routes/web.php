2a28c53cfef1d88512dc3d611db3eb4ddff14870
LionelMugisha <lionelmugisha2@gmail.com>
2022-01-13 10:34:02 +0200

    find composer package for post metadata

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,18 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    return view('posts',[
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('post/{post}', function ($slug) {
-    // return $slug;
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-    // ddd($path);
-    if(! file_exists($path)){
-        dd('404! File does not exist.');
-    }
-    $post = file_get_contents($path);
+    //find post by its slug and pass it to a view callled 'post'
     return view('post', [
-        'post' => $post
+        'post' => Post::find($slug)
     ]);
+
 })->where('post', '[A-z_\-]+'); 
