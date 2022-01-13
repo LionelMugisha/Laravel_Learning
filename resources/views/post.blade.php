b6733b3cf9ee655e7e89480ffc5b44b88a8adcd7
LionelMugisha <lionelmugisha2@gmail.com>
2022-01-13 11:50:16 +0200

    blade layouts 2 ways

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,21 +1,12 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <title>Blog</title>
-        <link rel="stylesheet" href="/app.css">
-    </head>
-    <body>
-
-        <article>
-            <h1>{{ $post->title }}</h1>
-            {{-- <h1>{!! $post->title !!}</h1> --}}
-            <div>
-                {{-- <?= $post->body; ?> --}}
-                {!! $post->body !!}
-            </div>
-        </article>
-
-        <a href="/">Go back</a>
-
-    </body>
-</html>
+<x-layout>
+    <article>
+        <h1>{{ $post->title }}</h1>
+        {{-- <h1>{!! $post->title !!}</h1> --}}
+        <div>
+            {{-- <?= $post->body; ?> --}}
+            {!! $post->body !!}
+        </div>
+    </article>
+    
+    <a href="/">Go back</a>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,24 +1,16 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <title>Blog</title>
-        <link rel="stylesheet" href="/app.css">
-    </head>
-    <body>
-        @foreach ($posts as $post)
-        @dd($loop)
-            <article class="{{ $loop->even ? 'foobar' : '' }}">
-                <h1>
-                    <a href="/post/{{$post->slug }}">
-                        <!-- ways to call elements from a collection -->
-                    {{-- <?= $post->title; ?> --}}  
-                    {{ $post->title }}
-                    </a>
-                </h1>
-                <div>
-                    {{ $post->excerpt }}
-                </div>
-            </article>
-        @endforeach
-    </body>
-</html>
+<x-layout>
+    @foreach ($posts as $post)
+        <article class="{{ $loop->even ? 'foobar' : '' }}">
+            <h1>
+                <a href="/post/{{$post->slug }}">
+                    <!-- ways to call elements from a collection -->
+                {{-- <?= $post->title; ?> --}}  
+                {{ $post->title }}
+                </a>
+            </h1>
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
+</x-layout>
