fe8569fa7f4bd9b6c8c2a90705c12f07faff0b68
LionelMugisha <lionelmugisha2@gmail.com>
2022-01-15 11:38:22 +0200

    clockwork

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
     return view('posts',[
-        'posts' => Post::all()
+        'posts' => Post::with('category')->get()
     ]);
 });
 
